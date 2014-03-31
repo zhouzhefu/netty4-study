e8e3d597cc0a406f814f6b2c1dc4f4f15ce6c355
Jeff Zhou <zhou.zhefu@gmail.com>
2014-03-31 15:17:42 +0800

    remove TimeClient and moved to another java project, because client and server cannot be launched within the same java instance, or connection will be refused.

--- src/main/java/com/jeff/study/netty4/App.java
+++ src/main/java/com/jeff/study/netty4/App.java
@@ -1,7 +1,6 @@
 package com.jeff.study.netty4;
 
 import com.jeff.study.netty4.discardserver.SimpleServer;
-import com.jeff.study.netty4.discardserver.TimeClient;
 
 /**
  * Hello world!
@@ -9,8 +8,6 @@ import com.jeff.study.netty4.discardserver.TimeClient;
  */
 public class App {
     public static void main( String[] args ) throws Exception {
-//        new Thread(new TimeClient()).start();
-        
         System.out.println("Netty Server starting.");
         SimpleServer.start();
     }
